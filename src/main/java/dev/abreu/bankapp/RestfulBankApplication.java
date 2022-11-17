65de7e7f08077e1f4addfff6d6425f467f8f3e35
Devin Abreu <devinabreu10@gmail.com>
2022-11-16 19:14:05 -0600

    Add extra dependencies to pom

--- src/main/java/dev/abreu/bankapp/RestfulBankApplication.java
+++ src/main/java/dev/abreu/bankapp/RestfulBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResTfulBankApplication {
+public class RestfulBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResTfulBankApplication.class, args);
+		SpringApplication.run(RestfulBankApplication.class, args);
 	}
 
 }
