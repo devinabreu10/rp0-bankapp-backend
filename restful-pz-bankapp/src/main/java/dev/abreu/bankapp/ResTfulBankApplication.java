2eccbaa15b85c5e114b823c166b1223f800ca63c
Devin Abreu <107730059+devinabreu10@users.noreply.github.com>
2022-11-16 18:52:19 -0600

    Add initial project files from start.spring.io

---
+++ restful-pz-bankapp/src/main/java/dev/abreu/bankapp/ResTfulBankApplication.java
@@ -0,0 +1,13 @@
+package dev.abreu.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulBankApplication.class, args);
+	}
+
+}
---
+++ restful-pz-bankapp/src/test/java/dev/abreu/bankapp/ResTfulBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abreu.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
