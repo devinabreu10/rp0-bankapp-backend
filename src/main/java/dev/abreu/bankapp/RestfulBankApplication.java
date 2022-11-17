7f664fe27bd60bdf4e3948d017ccfb29d492e0e7
Devin Abreu <devinabreu10@gmail.com>
2022-11-16 19:55:10 -0600

    update gitignore and pom.xml

--- src/main/java/dev/abreu/bankapp/RestfulBankApplication.java
+++ src/main/java/dev/abreu/bankapp/RestfulBankApplication.java
@@ -8,6 +8,16 @@ public class RestfulBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulBankApplication.class, args);
-	}
+		System.out.println(
+				",------.  ,-------.   ,-----.                     ,--.         ,---.                   \r\n"
+				+ "|  .--. ' `--.   /    |  |) /_   ,--,--. ,--,--,  |  |,-.     /  O  \\   ,---.   ,---.  \r\n"
+				+ "|  '--' |   /   /     |  .-.  \\ ' ,-.  | |      \\ |     /    |  .-.  | | .-. | | .-. | \r\n"
+				+ "|  | --'   /   `--.   |  '--' / \\ '-'  | |  ||  | |  \\  \\    |  | |  | | '-' ' | '-' ' \r\n"
+				+ "`--'      `-------'   `------'   `--`--' `--''--' `--'`--'   `--' `--' |  |-'  |  |-'  \r\n"
+				+ "                                                                       `--'    `--'    "
+				
+				);
+	} // need to figure out the database connection with Oracle SQL, I think I shut down the instance last time and 
+	  // don't remember to start it back up. After this you should setup a DAO class to test the DB connection with JDBC
 
 }
