9f225d0c2f6f46c042d751fc16dc9fb6b4e0b00a
Devin Abreu <devinabreu10@gmail.com>
2024-03-13 00:27:42 -0500

    added unit tests for Customer service/controller

---
+++ src/test/java/dev/abreu/bankapp/controllers/CustomerControllerTest.java
@@ -0,0 +1,182 @@
+package dev.abreu.bankapp.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.abreu.bankapp.dao.AccountDao;
+import dev.abreu.bankapp.dao.CustomerDao;
+import dev.abreu.bankapp.model.Customer;
+import dev.abreu.bankapp.service.CustomerService;
+
+@WebMvcTest(controllers = CustomerController.class)
+class CustomerControllerTest {
+	
+	@MockBean
+	private CustomerService customerService;
+	
+	@MockBean
+	private CustomerDao customerDao;
+	
+	@MockBean
+	private AccountDao accountDao;
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private ObjectMapper jsonMapper = new ObjectMapper();
+
+	@Test
+	void testGetCustomerByUsername() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.getCustomerByUsername(mockCustomer.getUsername())).thenReturn(mockCustomer);
+		
+		mockMvc.perform(get("/customer/get/user/testUsername"))
+				.andExpect(status().isOk())
+				.andExpect(content().json(jsonMapper.writeValueAsString(mockCustomer)));
+	}
+
+	@Test
+	void testGetCustomerById() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+
+		Mockito.when(customerService.getCustomerById(mockCustomer.getId())).thenReturn(mockCustomer);
+
+		mockMvc.perform(get("/customer/get/id/1"))
+				.andExpect(status().isOk())
+				.andExpect(content().json(jsonMapper.writeValueAsString(mockCustomer)));
+	}
+
+	@Test
+	void testGetAllCustomers() throws JsonProcessingException, Exception {
+		List<Customer> mockCustomerList = new ArrayList<>();
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		Customer mockCustomerTwo = new Customer(2L, "testFirstTwo", "testLastTwo", "testAddrTwo", "testUsernameTwo",
+				"testPasswordTwo");
+		mockCustomerList.add(mockCustomer);
+		mockCustomerList.add(mockCustomerTwo);
+
+		Mockito.when(customerService.getAllCustomers()).thenReturn(mockCustomerList);
+
+		mockMvc.perform(get("/customer"))
+				.andExpect(content().json(jsonMapper.writeValueAsString(mockCustomerList)));
+	}
+
+	@Test
+	void testSaveCustomer() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer();
+		Customer mockCustomerWithId = new Customer();
+		mockCustomerWithId.setId(1L);
+
+		Mockito.when(customerService.registerNewCustomer(mockCustomer)).thenReturn(mockCustomerWithId);
+
+		mockMvc.perform(post("/customer/save")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonMapper.writeValueAsString(mockCustomer)))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(jsonMapper.writeValueAsString(mockCustomerWithId)));
+	}
+
+	@Test
+	void testUpdateCustomer() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer();
+		mockCustomer.setId(1L);
+		
+		Mockito.when(customerService.updateCustomerDetails(mockCustomer)).thenReturn(mockCustomer);
+		
+		mockMvc.perform(put("/customer/update/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonMapper.writeValueAsString(mockCustomer)))
+				.andExpect(status().isOk())
+				.andExpect(content().json(jsonMapper.writeValueAsString(mockCustomer)));
+	}
+	
+	@Test
+	void testUpdateCustomerConflict() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		Customer mockCustomerUpdate = new Customer(1L, "newTestFirst", "newTestLast", "newTestAddr", "testUsername", "newTestPassword");
+		
+		Mockito.when(customerService.updateCustomerDetails(mockCustomer)).thenReturn(mockCustomerUpdate);
+		
+		mockMvc.perform(put("/customer/update/2")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonMapper.writeValueAsString(mockCustomer)))
+				.andExpect(status().isConflict());
+	}
+	
+	@Test
+	void testUpdateNullCustomer() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.updateCustomerDetails(mockCustomer)).thenReturn(null);
+		
+		mockMvc.perform(put("/customer/update/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonMapper.writeValueAsString(mockCustomer)))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void testDeleteCustomerByUsername() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.deleteCustomerByUsername(mockCustomer.getUsername())).thenReturn(Boolean.TRUE);
+		
+		mockMvc.perform(delete("/customer/delete/user/testUsername"))
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+	
+	@Test
+	void testUnsucessfulDeleteCustomerByUsername() throws JsonProcessingException, Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.deleteCustomerByUsername(mockCustomer.getUsername())).thenReturn(Boolean.FALSE);
+		
+		mockMvc.perform(delete("/customer/delete/user/testUsername"))
+				.andExpect(status().isConflict())
+				.andDo(print());
+	}
+
+	@Test
+	void testDeleteCustomerById() throws Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.deleteCustomerById(mockCustomer.getId())).thenReturn(Boolean.TRUE);
+		
+		mockMvc.perform(delete("/customer/delete/id/1"))
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+	
+	@Test
+	void testUnsucessfulDeleteCustomerById() throws Exception {
+		Customer mockCustomer = new Customer(1L, "testFirst", "testLast", "testAddr", "testUsername", "testPassword");
+		
+		Mockito.when(customerService.deleteCustomerById(mockCustomer.getId())).thenReturn(Boolean.FALSE);
+		
+		mockMvc.perform(delete("/customer/delete/id/1"))
+				.andExpect(status().isConflict())
+				.andDo(print());
+	}
+
+}
