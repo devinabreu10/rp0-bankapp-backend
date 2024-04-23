b46dbf78fad848fc00b1f825ba71cd6463ab2c8e
Devin Abreu <devinabreu10@gmail.com>
2024-04-23 10:53:26 -0500

    provided javadocs for dao interfaces with help of Codeium

--- src/main/java/dev/abreu/bankapp/dao/CustomerDao.java
+++ src/main/java/dev/abreu/bankapp/dao/CustomerDao.java
@@ -6,28 +6,75 @@ import java.util.Optional;
 import dev.abreu.bankapp.model.Customer;
 
 /**
- * This interface provides the basic CRUD operations for
- * the Customer model
+ * The CustomerDao interface provides methods for interacting with the Customer
+ * model in the database. It is responsible for finding, saving, updating, 
+ * and deleting Customer objects.
  * 
  * @author Devin Abreu
  *
  */
 public interface CustomerDao {
 
+	/**
+	 * Finds a Customer by username.
+	 *
+	 * @param username the username to search for
+	 * @return an Optional containing the Customer if found, or an empty Optional if not found
+	 */
 	Optional<Customer> findByUsername(String username);
-	
-	Optional<Customer> findById(Long customerId);
-	
-	List<Customer> findAllCustomers();
-	
-	boolean existsByUsername(String username);
-	
-	Customer saveCustomer(Customer customer);
-	
-	Customer updateCustomer(Customer customer);
-	
-	boolean deleteCustomerByUsername(String username);
-	
-	boolean deleteCustomerById(Long customerId);
 
+	/**
+	 * Finds a Customer by customer ID.
+	 *
+	 * @param customerId the ID of the Customer to search for
+	 * @return an Optional containing the Customer if found, or an empty Optional if not found
+	 */
+	Optional<Customer> findById(Long customerId);
+
+	/**
+	 * Finds all Customers in the database.
+	 *
+	 * @return a List of Customer objects
+	 */
+	List<Customer> findAllCustomers();
+
+	/**
+	 * Checks if a Customer with the given username exists in the database.
+	 *
+	 * @param username the username to check
+	 * @return true if a Customer with the given username exists, false otherwise
+	 */
+	boolean existsByUsername(String username);
+
+	/**
+	 * Saves a new Customer to the database.
+	 *
+	 * @param customer the Customer object to save
+	 * @return the saved Customer object
+	 */
+	Customer saveCustomer(Customer customer);
+
+	/**
+	 * Updates an existing Customer in the database.
+	 *
+	 * @param customer the Customer object to update
+	 * @return the updated Customer object
+	 */
+	Customer updateCustomer(Customer customer);
+
+	/**
+	 * Deletes a Customer by username.
+	 *
+	 * @param username the username of the Customer to delete
+	 * @return true if the Customer was deleted, false otherwise
+	 */
+	boolean deleteCustomerByUsername(String username);
+
+	/**
+	 * Deletes a Customer by customer ID.
+	 *
+	 * @param customerId the ID of the Customer to delete
+	 * @return true if the Customer was deleted, false otherwise
+	 */
+	boolean deleteCustomerById(Long customerId);
 }
--- src/main/java/dev/abreu/bankapp/dao/TransactionDao.java
+++ src/main/java/dev/abreu/bankapp/dao/TransactionDao.java
@@ -6,22 +6,52 @@ import java.util.Optional;
 import dev.abreu.bankapp.model.Transaction;
 
 /**
- * Along with using Spring Data JDBC I will also be using the JDBC API
- * for extra practice with both methods
+ * The TransactionDao interface provides methods for interacting with the
+ * Transaction model in the database. It is responsible for finding, saving,
+ * updating, and deleting Transaction objects.
  * 
  * @author Devin Abreu
  *
  */
 public interface TransactionDao {
-	
-	Optional<Transaction> findTransactionById(Long txnId);
-	
-	List<Transaction> findAllTransactionsByAcctNo(Long acctNo);
-	
-	Transaction saveTransaction(Transaction txn);
-	
-	Transaction updateTransaction(Transaction txn);
-	
-	boolean deleteTransactionById(Long txnId);
 
+	/**
+	 * Finds a Transaction by transaction ID.
+	 *
+	 * @param txnId the ID of the Transaction to search for
+	 * @return an Optional containing the Transaction if found, or an empty Optional if not found
+	 */
+	Optional<Transaction> findTransactionById(Long txnId);
+
+	/**
+	 * Finds all Transactions associated with a given account number.
+	 *
+	 * @param acctNo the account number to search for
+	 * @return a List of Transaction objects associated with the account number
+	 */
+	List<Transaction> findAllTransactionsByAcctNo(Long acctNo);
+
+	/**
+	 * Saves a new Transaction to the database.
+	 *
+	 * @param txn the Transaction object to save
+	 * @return the saved Transaction object
+	 */
+	Transaction saveTransaction(Transaction txn);
+
+	/**
+	 * Updates an existing Transaction in the database.
+	 *
+	 * @param txn the Transaction object to update
+	 * @return the updated Transaction object
+	 */
+	Transaction updateTransaction(Transaction txn);
+
+	/**
+	 * Deletes a Transaction by transaction ID.
+	 *
+	 * @param txnId the ID of the Transaction to delete
+	 * @return true if the Transaction was deleted, false otherwise
+	 */
+	boolean deleteTransactionById(Long txnId);
 }
