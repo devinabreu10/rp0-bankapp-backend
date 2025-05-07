d5d536417a5a41d42ca46fd3c85a7309b8ccae5e
Devin Abreu <devinabreu10@gmail.com>
2025-05-06 22:23:54 -0500

    refactor security to improve JWT handling and error response.

--- src/main/java/dev/abreu/bankapp/security/JwtAuthenticationFilter.java
+++ src/main/java/dev/abreu/bankapp/security/JwtAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package dev.abreu.bankapp.security;
 
-import java.io.IOException;
-
+import dev.abreu.bankapp.entity.Customer;
+import dev.abreu.bankapp.service.CustomerService;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,57 +15,61 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import dev.abreu.bankapp.entity.Customer;
-import dev.abreu.bankapp.service.CustomerService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-	
-	private final JwtConfig jwtConfig;
-	private final CustomerService customerService;
 
-	public JwtAuthenticationFilter(JwtConfig jwtConfig, CustomerService customerService) {
-		this.jwtConfig = jwtConfig;
-		this.customerService = customerService;
-	}
+    private final JwtConfig jwtConfig;
+    private final CustomerService customerService;
 
-	@Override
-	protected void doFilterInternal(
-			@NonNull HttpServletRequest request, 
-			@NonNull HttpServletResponse response, 
-			@NonNull FilterChain filterChain)
-			throws ServletException, IOException {
-		
-		final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-		final String jwt;
-		final String username;
-		
-		//if header is empty or doesn't include 'Bearer' then return
-		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
-			filterChain.doFilter(request, response);
-			return;
-		}
-		
-		jwt = authHeader.substring(7);
-		username = jwtConfig.extractUsername(jwt);
-		if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			Customer customer = customerService.getCustomerByUsername(username);
-			if(jwtConfig.isTokenValid(jwt, customer)) {
-				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-						customer, 
-						null,
-						customer.getAuthorities());
-				
-				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				
-				SecurityContextHolder.getContext().setAuthentication(authToken);
-			}
-		}
-		filterChain.doFilter(request, response);
-	}
+    public JwtAuthenticationFilter(JwtConfig jwtConfig, CustomerService customerService) {
+        this.jwtConfig = jwtConfig;
+        this.customerService = customerService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String username;
+
+        try {
+            //if header is empty or doesn't include 'Bearer' then return
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            jwt = authHeader.substring(7);
+            username = jwtConfig.extractUsername(jwt);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                Customer customer = customerService.getCustomerByUsername(username);
+                if (jwtConfig.isTokenValid(jwt, customer)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            customer,
+                            null,
+                            customer.getAuthorities());
+
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\":\"Access token expired, please login again.\"}");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
 
 }
